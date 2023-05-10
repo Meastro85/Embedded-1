f343ff7e9e2a55586aed54589506cfc5860b76e6
Meastro85 <verboven85@gmail.com>
2023-05-10 19:15:34 +0200

    Timer library finished(?)(EXPERIMENTAL)

--- Libraries/Timers/Timers.c
+++ Libraries/Timers/Timers.c
@@ -1,26 +1,53 @@
 #include <avr/io.h>
 
+#define TIMERS {TCCR0A, TCCR1A, TCCR2A}
+#define TIMERSB {TCCR0B, TCCR1B, TCCR2B}
+#define TIMERINTERRUPTS {TIMSK0, TIMSK1, TIMSK2}
+#define OCRXA {OCR0A, OCR1A, OCR2A}
+
 void initTimer(uint8_t timer, uint8_t mode, uint8_t interrupts) {
-  // vul aan
-if(timer == 0){
-
-} else if(timer == 1){
-
-} else if(timer == 2){
-    if(mode == 0){
-        TCCR2A |= _BV(WGM21);
-    } else if(mode == 1){
-        TCCR2A |= _BV(WGM20) | _BV(WGM21);
+    switch(mode){
+        case 0: TIMERS(timer) |= _BV(WGM01); break; //CSC Mode
+        case 1: TIMERS(timer) |= _BV(WGM00) | _BV(WGM21); break; //Fast PWM Mode
     }
-    if(interrupts == 0 || interrupts == 2){
-        TIMSK2 |= _BV(OCIE2A);
+    switch(interrupts){
+        case 0: TIMERINTERRUPTS(timer) |= _BV(OCIE0A); break; // OCRxA Interrupt
+        case 1: TIMERINTERRUPTS(timer) |= _BV(TOIE0); break; // Top Value Interrupt
+        case 2: TIMERINTERRUPTS(timer) |= _BV(OCIE0A); TIMERINTERRUPTS(timer) |= _BV(TOIE0); break; // Beide interrupts
     }
-    if(interrupts == 1 || interrupts == 2){
-        TIMSK2 |= _BV(TOIE2);
-    } 
 }
 
+void startTimer(uint8_t timer, uint8_t factor) {
+    switch(timer){
+        case 0: switch(factor){
+            case 1: TIMERSB(0) |= _BV(CS00); break;
+            case 8: TIMERSB(0) |= _BV(CS01); break;
+            case 64: TIMERSB(0) |= _BV(CS01) | _BV(CS00); break;
+            case 256: TIMERSB(0) |= _BV(CS02); break;
+            case 1024: TIMERSB(0) |= _BV(CS02) | _BV(CS00); break; 
+        }; break;
+        case 1: switch(factor){
+            case 1: TIMERSB(1) |= _BV(CS10); break;
+            case 8: TIMERSB(1) |= _BV(CS11); break;
+            case 64: TIMERSB(1) |= _BV(CS11) | _BV(CS10); break;
+            case 256: TIMERSB(1) |= _BV(CS12); break;
+            case 1024: TIMERSB(1) |= _BV(CS12) | _BV(CS10); break; 
+        }; break;
+        case 2: switch(factor){
+            case 1: TIMERSB(2) |= _BV(CS20); break;
+            case 8: TIMERSB(2) |= _BV(CS21); break;
+            case 64: TIMERSB(2) |= _BV(CS22); break;
+            case 128: TIMERSB(2) |= _BV(CS22) | _BV(CS20); break;
+            case 256: TIMERSB(2) |= _BV(CS22) | _BV(CS21); break;
+            case 1024: TIMERSB(2) |= _BV(CS22) | _BV(CS21) | _BV(CS20); break;
+        } break;
+    }
+}
 
+void stopTimer(uint8_t timer) {
+  TIMERSB(timer) &= ~(_BV(CS02) | _BV(CS01) | _BV(CS00));
+}
 
-
+void setOCRXA(uint8_t timer, int value){
+    OCRXA(timer) = value;
 }
--- Libraries/Timers/Timers.h
+++ Libraries/Timers/Timers.h
@@ -1,6 +1,21 @@
-void initTimer0();
-void initTimer1();
-void initTimer2();
-void setOCR0A(int* value);
-void setOCR1A(int* value);
-void setOCR2A(int* value);
+#include <avr/io.h>
+
+/// @brief Function to initialize a timer
+/// @param timer value of 0 to 2
+/// @param mode 0 = CSC Mode, 1 = Fast PWM Mode
+/// @param interrupts 0 = OCRxA interrupt, 1 = Top Value interrupt, 2 = both
+void initTimer(uint8_t timer, uint8_t mode, uint8_t interrupts);
+
+/// @brief Start a timer with the defined factor
+/// @param timer Value of 0 to 2
+/// @param factor Can be either 1, 8, 64, 128, 256 or 1024
+void startTimer(uint8_t timer, uint8_t factor);
+
+/// @brief Stop a timer
+/// @param timer value of 0 to 2
+void stopTimer(uint8_t timer);
+
+/// @brief Set the OCRxA interrupt value
+/// @param timer value of 0 to 2
+/// @param value value for the interrupt
+void setOCRXA(uint8_t timer, int value);
